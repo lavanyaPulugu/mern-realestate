fb9601a4991d6a9321b34dcc0f0eb38629c66d4c
lavanyapulugu <lavanya415@gmail.com>
2024-05-08 09:40:10 +0530

    add redux toolkit

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signinFailure,
+  signinSuccess,
+  signinStart,
+} from "../redux/user/userSlice";
 
 const Signin = () => {
   const [formData, setFormData] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
@@ -17,7 +23,7 @@ const Signin = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signinStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -27,16 +33,13 @@ const Signin = () => {
       });
       const data = await res.json();
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        dispatch(signinFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signinSuccess(data));
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(data.message);
+      dispatch(signinFailure(data.message));
     }
   };
 
